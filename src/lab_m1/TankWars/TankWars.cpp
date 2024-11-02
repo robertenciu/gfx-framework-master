277bc0a3df813fc9d65eb68ba8dc214914895740
robertenciu <robert_enciu@yahoo.com>
2024-11-02 20:10:28 +0200

    Initiated TankWars game files

---
+++ src/lab_m1/TankWars/TankWars.cpp
@@ -0,0 +1,167 @@
+#include "lab_m1/TankWars/TankWars.h"
+
+#include <vector>
+#include <iostream>
+
+#include "lab_m1/TankWars/transform2D.h"
+#include "lab_m1/TankWars/obj2D.h"
+
+using namespace std;
+using namespace m1;
+
+
+/*
+ *  To find out more about `FrameStart`, `Update`, `FrameEnd`
+ *  and the order in which they are called, see `world.cpp`.
+ */
+
+
+TankWars::TankWars()
+{
+}
+
+
+TankWars::~TankWars()
+{
+}
+
+
+void TankWars::Init()
+{
+    glm::ivec2 resolution = window->GetResolution();
+    auto camera = GetSceneCamera();
+    camera->SetOrthographic(0, (float)resolution.x, 0, (float)resolution.y, 0.01f, 400);
+    camera->SetPosition(glm::vec3(0, 0, 50));
+    camera->SetRotation(glm::vec3(0, 0, 0));
+    camera->Update();
+    GetCameraInput()->SetActive(false);
+
+    glm::vec3 corner = glm::vec3(0, 0, 0);
+    float squareSide = 100;
+
+    // TODO(student): Compute coordinates of a square's center, and store
+    // then in the `cx` and `cy` class variables (see the header). Use
+    // `corner` and `squareSide`. These two class variables will be used
+    // in the `Update()` function. Think about it, why do you need them?
+
+    // Initialize tx and ty (the translation steps)
+    translateX = 0;
+    translateY = 0;
+
+    // Initialize sx and sy (the scale factors)
+    scaleX = 1;
+    scaleY = 1;
+
+    // Initialize angularStep
+    angularStep = 0;
+
+    Mesh* square1 = obj2D::CreateSquare("square1", corner, squareSide, glm::vec3(1, 0, 0), true);
+    AddMeshToList(square1);
+
+    Mesh* square2 = obj2D::CreateSquare("square2", corner, squareSide, glm::vec3(0, 1, 0));
+    AddMeshToList(square2);
+
+    Mesh* square3 = obj2D::CreateSquare("square3", corner, squareSide, glm::vec3(0, 0, 1));
+    AddMeshToList(square3);
+}
+
+
+void TankWars::FrameStart()
+{
+    // Clears the color buffer (using the previously set color) and depth buffer
+    glClearColor(0, 0, 0, 1);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glm::ivec2 resolution = window->GetResolution();
+    // Sets the screen area where to draw
+    glViewport(0, 0, resolution.x, resolution.y);
+}
+
+
+void TankWars::Update(float deltaTimeSeconds)
+{
+    // TODO(student): Update steps for translation, rotation and scale,
+    // in order to create animations. Use the class variables in the
+    // class header, and if you need more of them to complete the task,
+    // add them over there!
+
+    modelMatrix = glm::mat3(1);
+    modelMatrix *= transform2D::Translate(150, 250);
+    // TODO(student): Create animations by multiplying the current
+    // transform matrix with the matrices you just implemented.
+    // Remember, the last matrix in the chain will take effect first!
+
+    RenderMesh2D(meshes["square1"], shaders["VertexColor"], modelMatrix);
+
+    modelMatrix = glm::mat3(1);
+    modelMatrix *= transform2D::Translate(400, 250);
+    // TODO(student): Create animations by multiplying the current
+    // transform matrix with the matrices you just implemented
+    // Remember, the last matrix in the chain will take effect first!
+
+    RenderMesh2D(meshes["square2"], shaders["VertexColor"], modelMatrix);
+
+    modelMatrix = glm::mat3(1);
+    modelMatrix *= transform2D::Translate(650, 250);
+    // TODO(student): Create animations by multiplying the current
+    // transform matrix with the matrices you just implemented
+    // Remember, the last matrix in the chain will take effect first!
+
+    RenderMesh2D(meshes["square3"], shaders["VertexColor"], modelMatrix);
+}
+
+
+void TankWars::FrameEnd()
+{
+}
+
+
+/*
+ *  These are callback functions. To find more about callbacks and
+ *  how they behave, see `input_controller.h`.
+ */
+
+
+void TankWars::OnInputUpdate(float deltaTime, int mods)
+{
+}
+
+
+void TankWars::OnKeyPress(int key, int mods)
+{
+    // Add key press event
+}
+
+
+void TankWars::OnKeyRelease(int key, int mods)
+{
+    // Add key release event
+}
+
+
+void TankWars::OnMouseMove(int mouseX, int mouseY, int deltaX, int deltaY)
+{
+    // Add mouse move event
+}
+
+
+void TankWars::OnMouseBtnPress(int mouseX, int mouseY, int button, int mods)
+{
+    // Add mouse button press event
+}
+
+
+void TankWars::OnMouseBtnRelease(int mouseX, int mouseY, int button, int mods)
+{
+    // Add mouse button release event
+}
+
+
+void TankWars::OnMouseScroll(int mouseX, int mouseY, int offsetX, int offsetY)
+{
+}
+
+
+void TankWars::OnWindowResize(int width, int height)
+{
+}
